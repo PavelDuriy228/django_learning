d94a964c0bdbae2d5604914db78fc3b1adb2c1eb
PavelDuriy228 <ii256018@gmail.com>
2025-11-03 13:02:11 +0300

    final

--- project/boards/views.py
+++ project/boards/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
+from .models import Bulletin, News
 
 def board_main(request):
     return render(request, 'board/index.html')
 
 def news_page(request):
-    return render(request, 'board/news.html')
+    news_list = News.objects.order_by("-created_at")
+    return render(request, 'board/news.html', {'news_list':news_list})
 
 def bullet_page(request):
-    return render(request, 'board/bullet.html')
+    bullet_list = Bulletin.objects.order_by("-created_at")
+    return render(request, 'board/bullet.html', {'bullet_list':bullet_list})
 
 def resp_page(request):
     return render(request, 'board/resp.html')
